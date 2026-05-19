895b4582cf6ed9110ed00159de4ba4cd86c03b21
Sara Damiano <sdamiano@stroudcenter.org>
2026-05-19 17:43:06 -0400

    Add docs to defines
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/SC16IS7XX.h
+++ src/SC16IS7XX.h
@@ -1,5 +1,3 @@
-#pragma once
-
 #ifndef _APPNOSTIC_SC16IS7XX_H_
 #define _APPNOSTIC_SC16IS7XX_H_
 
@@ -12,78 +10,146 @@
 #include <SPI.h>
 #include <Wire.h>
 
-// Device Address
+// Possible Device Addresses
+// Modified from Table 32. SC16IS752/SC16IS762 address map
+// NOTE: For the purposes of the Arduino Wire library, use the 7-bit address!
+
+/*
+| A1  | A0  | Binary     | Hex Address (W/R) | 7-Bit Address |
+|-----|-----|------------|-------------------|---------------|
+| VDD | VDD | 1001 000X  | 0x90 / 0x91       | 0x48          |
+| VDD | VSS | 1001 001X  | 0x92 / 0x93       | 0x49          |
+| VDD | SCL | 1001 010X  | 0x94 / 0x95       | 0x4A          |
+| VDD | SDA | 1001 011X  | 0x96 / 0x97       | 0x4B          |
+| VSS | VDD | 1001 100X  | 0x98 / 0x99       | 0x4C          |
+| VSS | VSS | 1001 101X  | 0x9A / 0x9B       | 0x4D          |
+| VSS | SCL | 1001 110X  | 0x9C / 0x9D       | 0x4E          |
+| VSS | SDA | 1001 111X  | 0x9E / 0x9F       | 0x4F          |
+| SCL | VDD | 1010 000X  | 0xA0 / 0xA1       | 0x50          |
+| SCL | VSS | 1010 001X  | 0xA2 / 0xA3       | 0x51          |
+| SCL | SCL | 1010 010X  | 0xA4 / 0xA5       | 0x52          |
+| SCL | SDA | 1010 011X  | 0xA6 / 0xA7       | 0x53          |
+| SDA | VDD | 1010 100X  | 0xA8 / 0xA9       | 0x54          |
+| SDA | VSS | 1010 101X  | 0xAA / 0xAB       | 0x55          |
+| SDA | SCL | 1010 110X  | 0xAC / 0xAD       | 0x56          |
+| SDA | SDA | 1010 111X  | 0xAE / 0xAF       | 0x57          |
+
+**Note:** X = logic 0 for write cycle; X = logic 1 for read cycle.
+*/
+
 
 // A:VDD
 // B:GND
 // C:SCL
 // D:SDA
-#define SC16IS7XX_ADDRESS_AA (0X90)
-#define SC16IS7XX_ADDRESS_AB (0X92)
-#define SC16IS7XX_ADDRESS_AC (0X94)
-#define SC16IS7XX_ADDRESS_AD (0X96)
-#define SC16IS7XX_ADDRESS_BA (0X98)
-#define SC16IS7XX_ADDRESS_BB (0X9A)
-#define SC16IS7XX_ADDRESS_BC (0X9C)
-#define SC16IS7XX_ADDRESS_BD (0X9E)
-#define SC16IS7XX_ADDRESS_CA (0XA0)
-#define SC16IS7XX_ADDRESS_CB (0XA2)
-#define SC16IS7XX_ADDRESS_CC (0XA4)
-#define SC16IS7XX_ADDRESS_CD (0XA6)
-#define SC16IS7XX_ADDRESS_DA (0XA8)
-#define SC16IS7XX_ADDRESS_DB (0XAA)
-#define SC16IS7XX_ADDRESS_DC (0XAC)
-#define SC16IS7XX_ADDRESS_DD (0XAE)
+#define SC16IS7XX_ADDRESS_AA (0X90)  ///< Both A and B tied to VDD
+#define SC16IS7XX_ADDRESS_AB (0X92)  ///< A tied to VDD, B tied to GND
+#define SC16IS7XX_ADDRESS_AC (0X94)  ///< A tied to VDD, B tied to SCL
+#define SC16IS7XX_ADDRESS_AD (0X96)  ///< A tied to VDD, B tied to SDA
+#define SC16IS7XX_ADDRESS_BA (0X98)  ///< A tied to GND, B tied to VDD
+#define SC16IS7XX_ADDRESS_BB (0X9A)  ///< A tied to GND, B tied to GND
+#define SC16IS7XX_ADDRESS_BC (0X9C)  ///< A tied to GND, B tied to SCL
+#define SC16IS7XX_ADDRESS_BD (0X9E)  ///< A tied to GND, B tied to SDA
+#define SC16IS7XX_ADDRESS_CA (0XA0)  ///< A tied to SCL, B tied to VDD
+#define SC16IS7XX_ADDRESS_CB (0XA2)  ///< A tied to SCL, B tied to GND
+#define SC16IS7XX_ADDRESS_CC (0XA4)  ///< A tied to SCL, B tied to SCL
+#define SC16IS7XX_ADDRESS_CD (0XA6)  ///< A tied to SCL, B tied to SDA
+#define SC16IS7XX_ADDRESS_DA (0XA8)  ///< A tied to SDA, B tied to VDD
+#define SC16IS7XX_ADDRESS_DB (0XAA)  ///< A tied to SDA, B tied to GND
+#define SC16IS7XX_ADDRESS_DC (0XAC)  ///< A tied to SDA, B tied to SCL
+#define SC16IS7XX_ADDRESS_DD (0XAE)  ///< A tied to SDA, B tied to SDA
 
 // General Registers
-#define SC16IS7XX_REG_RHR (0x00)
-#define SC16IS7XX_REG_THR (0X00)
-#define SC16IS7XX_REG_IER (0X01)
-#define SC16IS7XX_REG_FCR (0X02)
-#define SC16IS7XX_REG_IIR (0X02)
-#define SC16IS7XX_REG_LCR (0X03)
-#define SC16IS7XX_REG_MCR (0X04)
-#define SC16IS7XX_REG_LSR (0X05)
-#define SC16IS7XX_REG_MSR (0X06)
-#define SC16IS7XX_REG_SPR (0X07)
-#define SC16IS7XX_REG_TCR (0X06)
-#define SC16IS7XX_REG_TLR (0X07)
-#define SC16IS7XX_REG_TXLVL (0X08)
-#define SC16IS7XX_REG_RXLVL (0X09)
-#define SC16IS7XX_REG_IODIR (0X0A)
-#define SC16IS7XX_REG_IOSTATE (0X0B)
-#define SC16IS7XX_REG_IOINTENA (0X0C)
-#define SC16IS7XX_REG_IOCONTROL (0X0E)
-#define SC16IS7XX_REG_EFCR (0X0F)
+
+// clang-format off
+/*
+| Bit(s) | Name     | Function                                                      |
+|--------|----------|---------------------------------------------------------------|
+| 7      | -        | Not used                                                      |
+| 6:3    | A[3:0]   | UART’s internal register select                               |
+| 2:1    | CH1, CH0 | Channel select [00 = A, 01 = B, 10 = reserved, 11 = reserved] |
+| 0      | -        | Not used                                                      |
+*/
+// clang-format on
+
+// NOTE: Because the address is shifted left by 3 bits and the channel is
+// shifted left by 1 bit, the register address to use in the Wire functions is
+// effectively (reg_addr << 3 | channel << 1) when writing or reading registers
+// for a specific channel on the SC16IS752.
+
+#define SC16IS7XX_REG_RHR (0x00)  ///< Receiver Holding Register (read only)
+#define SC16IS7XX_REG_THR (0X00)  ///< Transmit Holding Register (write only)
+#define SC16IS7XX_REG_IER (0X01)  ///< Interrupt Enable Register
+#define SC16IS7XX_REG_FCR (0X02)  ///< FIFO Control Register (write only)
+#define SC16IS7XX_REG_IIR \
+    (0X02)  ///< Interrupt Identification Register (read only)
+#define SC16IS7XX_REG_LCR (0X03)  ///< Line Control Register
+#define SC16IS7XX_REG_MCR (0X04)  ///< Modem Control Register
+#define SC16IS7XX_REG_LSR (0X05)  ///< Line Status Register (read only)
+#define SC16IS7XX_REG_MSR (0X06)  ///< Modem Status Register (read only)
+#define SC16IS7XX_REG_SPR (0X07)  ///< Scratchpad Register
+#define SC16IS7XX_REG_TCR (0X06)  ///< Transmission Control Register
+#define SC16IS7XX_REG_TLR (0X07)  ///< Trigger Level Register
+#define SC16IS7XX_REG_TXLVL \
+    (0X08)  ///< Transmit FIFO Level Register (read only)
+#define SC16IS7XX_REG_RXLVL (0X09)  ///< Receive FIFO Level Register (read only)
+#define SC16IS7XX_REG_IODIR (0X0A)  ///< I/O Direction Register
+#define SC16IS7XX_REG_IOSTATE (0X0B)    ///< I/O State Register
+#define SC16IS7XX_REG_IOINTENA (0X0C)   ///< I/O Interrupt Enable Register
+#define SC16IS7XX_REG_IOCONTROL (0X0E)  ///< I/O Control Register
+#define SC16IS7XX_REG_EFCR (0X0F)       ///< Extra Features Control Register
 
 // Special Registers
-#define SC16IS7XX_REG_DLL (0x00)
-#define SC16IS7XX_REG_DLH (0X01)
+#define SC16IS7XX_REG_DLL (0x00)  ///< Divisor Latch LSB
+#define SC16IS7XX_REG_DLH (0X01)  ///< Divisor Latch MSB
 
 // Enhanced Registers
-#define SC16IS7XX_REG_EFR (0X02)
-#define SC16IS7XX_REG_XON1 (0X04)
-#define SC16IS7XX_REG_XON2 (0X05)
-#define SC16IS7XX_REG_XOFF1 (0X06)
-#define SC16IS7XX_REG_XOFF2 (0X07)
+#define SC16IS7XX_REG_EFR (0X02)    ///< Enhanced Feature Register
+#define SC16IS7XX_REG_XON1 (0X04)   ///< XON1 Register
+#define SC16IS7XX_REG_XON2 (0X05)   ///< XON2 Register
+#define SC16IS7XX_REG_XOFF1 (0X06)  ///< XOFF1 Register
+#define SC16IS7XX_REG_XOFF2 (0X07)  ///< XOFF2 Register
 
-//
-#define SC16IS7XX_INT_CTSRTS (0X20)
-#define SC16IS7XX_INT_GPIO (0x30)
-#define SC16IS7XX_INT_XOFF (0X10)
-#define SC16IS7XX_INT_TIMEOUT (0X0c)
-#define SC16IS7XX_INT_MODEM (0X00)
-#define SC16IS7XX_INT_LINE (0X06)
-#define SC16IS7XX_INT_THR (0X02)
-#define SC16IS7XX_INT_RHR (0X04)
+// Interrupt Enable Register Bits
+// Taken from datasheet table 11
+#define SC16IS7XX_IER_CTS (0X07)    ///< CTS Interrupt Enable
+#define SC16IS7XX_IER_RTS (0X06)    ///< RTS Interrupt Enable
+#define SC16IS7XX_IER_XOFF (0X05)   ///< XOFF Interrupt
+#define SC16IS7XX_IER_SLEEP (0X04)  ///< Sleep Interrupt
+#define SC16IS7XX_IER_MODEM (0X03)  ///< Modem Interrupt
+// NOTE: The modem interrupt is triggered by changes in the RI, CD, or DSR pins
+// **if** the I/O pins are configured as modem pins The modem interrupt is also
+// triggered by changes in the GPIO pins **if** the I/O pins are configured as
+// GPIO pins
+#define SC16IS7XX_IER_RLS (0X02)  ///< Receiver Line Status Interrupt
+#define SC16IS7XX_IER_THR (0X01)  ///< Transmit Holding Register Interrupt
+#define SC16IS7XX_IER_RHR (0X00)  ///< Receive Holding Register Interrupt
+
+// Interrupt Identification Register Values
+// Taken from datasheet table 14
+// Listed from increasing to decreasing priority
+#define SC16IS7XX_INT_LINE (0X06)     ///< Line Interrupt 0b00000110
+#define SC16IS7XX_INT_TIMEOUT (0X0c)  ///< Timeout Interrupt 0b00001100
+#define SC16IS7XX_INT_RHR \
+    (0X04)  ///< Receive Holding Register Interrupt 0b00000100
+#define SC16IS7XX_INT_THR \
+    (0X02)  ///< Transmit Holding Register Interrupt 0b00000010
+#define SC16IS7XX_INT_MODEM (0X00)   ///< Modem Interrupt 0b00000000
+#define SC16IS7XX_INT_GPIO (0x30)    ///< GPIO Interrupt 0b00110000
+#define SC16IS7XX_INT_XOFF (0X10)    ///< XOFF Interrupt 0b00010000
+#define SC16IS7XX_INT_CTSRTS (0X20)  ///< CTS/RTS Interrupt 0b00100000
 
 // Application Related
-
-#define SC16IS7XX_XTAL_FREQ (14745600UL)
 #define SC16IS7XX_PROTOCOL_I2C (0)
 #define SC16IS7XX_PROTOCOL_SPI (1)
+#define SC16IS7XX_SPIREG ADDRBIT8_HIGH_TOREAD  ///< SPI register type
 
-typedef enum { MODEM_PIN_GPIO_0 = 0, MODEM_PIN_GPIO_1 = 1 } modem_gpio_t;
+// Default values
+#define SC16IS7XX_DEFAULT_ADDRESS \
+    (SC16IS7XX_ADDRESS_AA)                     ///< The default I2C address
+#define SC16IS7XX_DEFAULT_SPIFREQ (1000000UL)  ///< The default SPI Clock speed
+#define SC16IS7XX_DEFAULT_XTAL_FREQ \
+    (14745600UL)  ///< The default frequency of the crystal in hertz
 
 /**
  * @brief Base driver for SC16IS7XX family devices using I2C or SPI.
