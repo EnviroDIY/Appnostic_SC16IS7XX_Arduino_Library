e1be2d1bf92cdadb437d3044c65a64b756ead99e
Sara Damiano <sdamiano@stroudcenter.org>
2026-05-20 17:50:45 -0400

    Fix missing doc
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/SC16IS7XX.h
+++ src/SC16IS7XX.h
@@ -375,7 +375,8 @@ class SC16IS7XX {
 
     void storeCallback(uint16_t callbackMask, voidFxnPtr callback);
     void clearCallback(uint16_t callbackMask);
-    // static pointer to active SC16IS7XX instance, needed for easy ISR handling
+    /// static pointer to active SC16IS7XX instance, needed for easy ISR
+    /// handling
     static SC16IS7XX* _activeObject;
 
  public:
