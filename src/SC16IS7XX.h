621d84bd28211438c05485174a70de9087e4cdc7
Sara Damiano <sdamiano@stroudcenter.org>
2026-05-20 12:16:25 -0400

    group docs
    Signed-off-by: Sara Damiano <sdamiano@stroudcenter.org>

--- src/SC16IS7XX.h
+++ src/SC16IS7XX.h
@@ -11,34 +11,55 @@
 #include <Adafruit_I2CDevice.h>
 #include <Adafruit_SPIDevice.h>
 
-// Possible Device Addresses
-// Modified from Table 32. SC16IS752/SC16IS762 address map
-// NOTE: For the purposes of the Arduino Wire library, use the 7-bit address!
 
-/*
-| A1  | A0  | Binary     | Hex Address (W/R) | 7-Bit Address |
-|-----|-----|------------|-------------------|---------------|
-| VDD | VDD | 1001 000X  | 0x90 / 0x91       | 0x48          |
-| VDD | VSS | 1001 001X  | 0x92 / 0x93       | 0x49          |
-| VDD | SCL | 1001 010X  | 0x94 / 0x95       | 0x4A          |
-| VDD | SDA | 1001 011X  | 0x96 / 0x97       | 0x4B          |
-| VSS | VDD | 1001 100X  | 0x98 / 0x99       | 0x4C          |
-| VSS | VSS | 1001 101X  | 0x9A / 0x9B       | 0x4D          |
-| VSS | SCL | 1001 110X  | 0x9C / 0x9D       | 0x4E          |
-| VSS | SDA | 1001 111X  | 0x9E / 0x9F       | 0x4F          |
-| SCL | VDD | 1010 000X  | 0xA0 / 0xA1       | 0x50          |
-| SCL | VSS | 1010 001X  | 0xA2 / 0xA3       | 0x51          |
-| SCL | SCL | 1010 010X  | 0xA4 / 0xA5       | 0x52          |
-| SCL | SDA | 1010 011X  | 0xA6 / 0xA7       | 0x53          |
-| SDA | VDD | 1010 100X  | 0xA8 / 0xA9       | 0x54          |
-| SDA | VSS | 1010 101X  | 0xAA / 0xAB       | 0x55          |
-| SDA | SCL | 1010 110X  | 0xAC / 0xAD       | 0x56          |
-| SDA | SDA | 1010 111X  | 0xAE / 0xAF       | 0x57          |
+/**
+ * @anchor chip_features
+ * @name Chip Features
+ * Features of the SC16IS7XX family of devices.
+ */
+/**@{*/
+#define SC16IS7XX_UART_CHANNELS (2)  ///< Number of UART channels on the device
+#define SC16IS7XX_GPIO_PINS (8)      ///< Number of GPIO pins on the device
+#define SC16IS7XX_NON_GPIO_INTERRUPTS \
+    (11)  ///< The number of non-pin-related interrupt sources (DSR, DTR, CD,
+          ///< RI,
+          ///< CTS, RTS, XOFF, RLS, RHR, THR, Timeout) on the device. These
+///< interrupts are shared between both channels and are not related to
+///< the GPIO pins.
+#define SC16IS7XX_SPIREG ADDRBIT8_HIGH_TOREAD  ///< SPI register type
+/**@}*/
 
-**Note:** X = logic 0 for write cycle; X = logic 1 for read cycle.
-*/
-
-
+/**
+ * @anchor device_addresses
+ * @name Device Addresses
+ * Possible I2C addresses for the SC16IS7XX family of devices.
+ *
+ * Modified from Table 32. SC16IS752/SC16IS762 address map
+ *
+ * @note For the purposes of the Arduino Wire library, use the 7-bit address!
+ *
+ * | A1  | A0  | Binary**   | Hex Address (W/R) | 7-Bit Address |
+ * |-----|-----|------------|-------------------|---------------|
+ * | VDD | VDD | 1001 000X  | 0x90 / 0x91       | 0x48          |
+ * | VDD | VSS | 1001 001X  | 0x92 / 0x93       | 0x49          |
+ * | VDD | SCL | 1001 010X  | 0x94 / 0x95       | 0x4A          |
+ * | VDD | SDA | 1001 011X  | 0x96 / 0x97       | 0x4B          |
+ * | VSS | VDD | 1001 100X  | 0x98 / 0x99       | 0x4C          |
+ * | VSS | VSS | 1001 101X  | 0x9A / 0x9B       | 0x4D          |
+ * | VSS | SCL | 1001 110X  | 0x9C / 0x9D       | 0x4E          |
+ * | VSS | SDA | 1001 111X  | 0x9E / 0x9F       | 0x4F          |
+ * | SCL | VDD | 1010 000X  | 0xA0 / 0xA1       | 0x50          |
+ * | SCL | VSS | 1010 001X  | 0xA2 / 0xA3       | 0x51          |
+ * | SCL | SCL | 1010 010X  | 0xA4 / 0xA5       | 0x52          |
+ * | SCL | SDA | 1010 011X  | 0xA6 / 0xA7       | 0x53          |
+ * | SDA | VDD | 1010 100X  | 0xA8 / 0xA9       | 0x54          |
+ * | SDA | VSS | 1010 101X  | 0xAA / 0xAB       | 0x55          |
+ * | SDA | SCL | 1010 110X  | 0xAC / 0xAD       | 0x56          |
+ * | SDA | SDA | 1010 111X  | 0xAE / 0xAF       | 0x57          |
+ *
+ * ** X = logic 0 for write cycle; X = logic 1 for read cycle.
+ */
+/**@{*/
 // A:VDD
 // B:GND
 // C:SCL
@@ -59,11 +80,14 @@
 #define SC16IS7XX_ADDRESS_DB (0XAA)  ///< A tied to SDA, B tied to GND
 #define SC16IS7XX_ADDRESS_DC (0XAC)  ///< A tied to SDA, B tied to SCL
 #define SC16IS7XX_ADDRESS_DD (0XAE)  ///< A tied to SDA, B tied to SDA
+/**@}*/
 
-// General Registers
-
+/**
+ * @anchor registers
+ * @name Registers
+ */
 // clang-format off
-/*
+/**
 | Bit(s) | Name     | Function                                                      |
 |--------|----------|---------------------------------------------------------------|
 | 7      | -        | Not used                                                      |
@@ -72,12 +96,14 @@
 | 0      | -        | Not used                                                      |
 */
 // clang-format on
-
-// NOTE: Because the address is shifted left by 3 bits and the channel is
-// shifted left by 1 bit, the register address to use in the Wire functions is
-// effectively (reg_addr << 3 | channel << 1) when writing or reading registers
-// for a specific channel on the SC16IS752.
-
+/**
+ * @note Because the address is shifted left by 3 bits and the channel is
+ * shifted left by 1 bit, the register address to use in the Wire functions is
+ * effectively (reg_addr << 3 | channel << 1) when writing or reading registers
+ * for a specific channel on the SC16IS752.
+ */
+/**@{*/
+// General registers
 #define SC16IS7XX_REG_RHR (0x00)  ///< Receiver Holding Register (read only)
 #define SC16IS7XX_REG_THR (0X00)  ///< Transmit Holding Register (write only)
 #define SC16IS7XX_REG_IER (0X01)  ///< Interrupt Enable Register
@@ -110,36 +136,48 @@
 #define SC16IS7XX_REG_XON2 (0X05)   ///< XON2 Register
 #define SC16IS7XX_REG_XOFF1 (0X06)  ///< XOFF1 Register
 #define SC16IS7XX_REG_XOFF2 (0X07)  ///< XOFF2 Register
+/**@}*/
 
-// Interrupt Enable Register Bits
 
-// NOTE:
-// - Enabling any of the MODEM, RLS, THR, or RHR interrupts put the module
-// into "Interrupt Mode Operation".  In this mode, the host is informted of the
-// status of the rx and tx via the interrupt signal (the IRQ pin) and should
-// check the IIR register for the source of the interrupt.
-// - If you do NOT enable any of the MODEM, RLS, THR, or RHR interrupts, then
-// the module is in "Polling Mode Operation" and the host should check the LSR
-// and MSR registers to check the status of the rx and tx.
-// - Enabling the CTS, RTS, or XOFF interrupts does NOT put the module into
-// "Interrupt Mode Operation". The host should check the IIR register for the
-// source of the interrupt if any of these are enabled, but they can be enabled
-// in either mode of operation.
-
-// Taken from datasheet table 11
+/**
+ * @anchor interrupt_bits
+ * @name Interrupt Enable Register Bits
+ *
+ * Taken from datasheet table 11
+ *
+ * @note
+ * - Enabling any of the MODEM, RLS, THR, or RHR interrupts put the module into
+ * "Interrupt Mode Operation".  In this mode, the host is informted of the
+ * status of the rx and tx via the interrupt signal (the IRQ pin) and should
+ * check the IIR register for the source of the interrupt. *
+ * - If you do NOT enable any of the MODEM, RLS, THR, or RHR interrupts, then
+ * the module is in "Polling Mode Operation" and the host should check the LSR
+ * and MSR registers to check the status of the rx and tx. *
+ * - Enabling the CTS, RTS, or XOFF interrupts does NOT put the module into
+ * "Interrupt Mode Operation". The host should check the IIR register for the
+ * source of the interrupt if any of these are enabled, but they can be enabled
+ * in either mode of operation.
+ */
+/**@{*/
 #define SC16IS7XX_IER_CTS (0X07)    ///< CTS Interrupt Enable
 #define SC16IS7XX_IER_RTS (0X06)    ///< RTS Interrupt Enable
 #define SC16IS7XX_IER_XOFF (0X05)   ///< XOFF Interrupt
 #define SC16IS7XX_IER_SLEEP (0X04)  ///< Sleep Mode Enable (NOT AN INTERRUPT)
 #define SC16IS7XX_IER_MODEM (0X03)  ///< Modem Interrupt
-// NOTE: The modem interrupt is triggered by changes in the RI, CD/DTR, or DSR
-// pins **if** the I/O pins are configured as modem pins.
+///< @note The modem interrupt is triggered by changes in the RI, CD, DTR, or
+///< DSR pins **if and only if** the I/O pins are configured as modem pins.
 #define SC16IS7XX_IER_RLS (0X02)  ///< Receiver Line Status Interrupt
 #define SC16IS7XX_IER_THR (0X01)  ///< Transmit Holding Register Interrupt
 #define SC16IS7XX_IER_RHR (0X00)  ///< Receive Holding Register Interrupt
+/**@}*/
 
-// Interrupt Identification Register Values
-// Taken from datasheet table 14
+/**
+ * @anchor interrupt_sources
+ * @name Interrupt Identification Register Values
+ *
+ * Taken from datasheet table 14
+ */
+/**@{*/
 // Listed from increasing to decreasing priority
 #define SC16IS7XX_INT_LINE (0X06)     ///< Line Interrupt 0b00000110
 #define SC16IS7XX_INT_TIMEOUT (0X0c)  ///< Timeout Interrupt 0b00001100
@@ -151,18 +189,17 @@
 #define SC16IS7XX_INT_GPIO (0x30)    ///< GPIO Interrupt 0b00110000
 #define SC16IS7XX_INT_XOFF (0X10)    ///< XOFF Interrupt 0b00010000
 #define SC16IS7XX_INT_CTSRTS (0X20)  ///< CTS/RTS Interrupt 0b00100000
-
-// Application Related
-#define SC16IS7XX_PROTOCOL_I2C (0)
-#define SC16IS7XX_PROTOCOL_SPI (1)
-#define SC16IS7XX_SPIREG ADDRBIT8_HIGH_TOREAD  ///< SPI register type
-
-// Default values
+/**
+ * @anchor default_values
+ * @name Default Values
+ */
+/**@{*/
 #define SC16IS7XX_DEFAULT_ADDRESS \
     (SC16IS7XX_ADDRESS_AA)                     ///< The default I2C address
 #define SC16IS7XX_DEFAULT_SPIFREQ (1000000UL)  ///< The default SPI Clock speed
 #define SC16IS7XX_DEFAULT_XTAL_FREQ \
     (14745600UL)  ///< The default frequency of the crystal in hertz
+/**@}*/
 
 /**
  * @brief Base driver for SC16IS7XX family devices using I2C or SPI.
